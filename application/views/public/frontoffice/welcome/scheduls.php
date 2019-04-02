a1d799e0a36f69142c31b287d502151c5d4f5223
te_chaiwat <te.chaiwat@gmail.com>
2019-04-02 15:59:56 +0700

    update scheduls

--- application/views/public/frontoffice/welcome/scheduls.php
+++ application/views/public/frontoffice/welcome/scheduls.php
@@ -3,43 +3,45 @@
 	<div class="row">
 		<div class="col-md-12">
 			<div class="card">
-				<div id="headingRegisOne">
-					<div class="collapsed card-header" data-toggle="collapse" data-target="#headingRegis" aria-expanded="false" aria-controls="headingRegis">
-
-						<span class="time">08.30 - 09.00 น.</span>
-						<h4>ลงทะเบียน</h4>
-
-					</div>
-				</div>
-				<div id="headingRegis" class="collapse show" aria-labelledby="headingRegisOne" data-parent="#accordionRegiser">
-					<div class="card-body">
-						<p>09.00 - 09.30 น.</p>
-						<div class="location">
-							พิธีเปิดการประชุมโดยอธิการบดี มหาวิทยาลัยราชภัฏอุดรธานีี ณ ห้องประชุม 1 ชั้น 3 อาคาร SCB1
-						</div>
-						<p>09.30 - 10.30 น.</p>
-						<div class="location">
-							บรรยายพิเศษจากผู้ทรงคุณวุฒิ
-						</div>
-						<p>10.30 - 12.00 น.</p>
-						<div class="location">
-							การนำเสนอแบบบรรยาย ช่วงที่ 1
-						</div>
-						<p>12.00 - 13.00 น.</p>
-						<div class="location">
-							พักรับประทานอาหารกลางวัน
-						</div>
-						<p>13.00 - 15.00 น.</p>
-						<div class="location">
-							การนำเสนอแบบบรรยาย ช่วงที่ 2
-						</div>
-						<p>15.00 - 17.00 น.</p>
-						<div class="location">
-							การนำเสนอแบบโปสเตอร์
-						</div>
-					</div>
-				</div>
-			</div>
+								<div id="headingRegisOne">
+									<div class="collapsed card-header" data-toggle="collapse" data-target="#headingRegis" aria-expanded="false" aria-controls="headingRegis">
+										<span class="time">08.30 - 09.00 น.</span>
+										<h4>ลงทะเบียน</h4>
+									</div>
+								</div>
+								<div id="headingRegis" class="collapse show" aria-labelledby="headingRegisOne" data-parent="#accordionRegiser">
+									<div class="card-body">
+										<p>09.00 - 09.30 น.</p>
+										<div class="location">
+											พิธีเปิดการประชุมโดยอธิการบดี มหาวิทยาลัยราชภัฏอุดรธานีี ณ ห้องประชุม 1 ชั้น 3 อาคาร SCB1
+										</div>
+										<p>09.30 - 09.45 น.</p>
+										<div class="location">
+											พิธีมอบโล่แก่เจ้าภาพร่วม และ ถ่ายภาพ
+										</div>
+										<p>09.45 - 10.30 น.</p>
+										<div class="location">
+											พิธีเปิดนิทัศการผลงานภาคโปสเตอร์โดยอธิการบดี  มหาวิทยาลัยราชภัฏอุดรธานี พร้อมตัวแทนเจ้าภาพร่วม  คณาจารย์  นักศึกษา  และ  ผู้ที่มาร่วมงาน  ร่วมชมนิทัศการ
+										</div>
+										<p>10.30 - 12.00 น.</p>
+										<div class="location">
+											การบรรยายพิเศษจากผู้ทรงคุณวุฒิ
+										</div>
+										<p>12.00 - 13.00 น.</p>
+										<div class="location">
+											พักรับประทานอาหารกลางวัน
+										</div>
+										<p>13.00 - 16.00 น.</p>
+										<div class="location">
+											การนำเสนอแบบบรรยาย / โปสเตอร์
+										</div>
+										<p>16.00 - 17.00 น.</p>
+										<div class="location">
+											พิธีมอบรางวัลและพิธีปิดการประชุมโดยคณบดีคณะวิทยาศาสตร์
+										</div>
+									</div>
+								</div>
+							</div>
 			<div class="card">
 				<div id="headingRegisTwo">
 					<div class="collapsed card-header" data-toggle="collapse" data-target="#" aria-expanded="false" aria-controls="">
